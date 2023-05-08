bd457d7fe0511497069dc066f4d114b9288ba673
Jinn_o <32761189+SMJin@users.noreply.github.com>
2023-05-08 17:56:27 +0900

    Update initial_code.py - 1급 1차 2번
    실제 문제

--- 1급/1차/2번/initial_code.py
+++ 1급/1차/2번/initial_code.py
@@ -1,23 +1,25 @@
-def addZeroFirst(binaryAB, cnt):
-  for i in range(cnt):
-    binaryAB = '0' + binaryAB
-  return binaryAB
+def func_a(string, length):
+    padZero = ""
+    padSize = @@@
+    for i in range(padSize):
+        padZero += "0"
+    return padZero + string
 
-def hammingDistance(binaryA, binaryB):
-  lenA = len(binaryA)
-  lenB = len(binaryB)
-  if lenA > lenB:
-    len_diff = lenA - lenB
-    addZeroFirst(lenB, len_diff)
-  elif lenA < lenB:
-    len_diff = lenB - lenA
-    addZeroFirst(lenA, len_diff)
-  
-  cnt = 0
-  for i in range(lenA):
-    if binaryA[i] != binaryB[i]:
-      cnt = cnt + 1
-  return cnt      
-      
 def solution(binaryA, binaryB):
-  hammingDistance(binaryA, binaryB)
+    max_length = max(len(binaryA), len(binaryB))
+    binaryA = func_a(binaryA, max_length)
+    binaryB = func_a(binaryB, max_length)
+    
+    hamming_distance = 0
+    for i in range(max_length):
+        if @@@:
+            hamming_distance += 1
+    return hamming_distance
+
+#The following is code to output testcase.
+binaryA = "10010"
+binaryB = "110"
+ret = solution(binaryA, binaryB)
+
+#Press Run button to receive output. 
+print("Solution: return value of the function is", ret, ".")
